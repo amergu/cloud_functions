0754529521f8cca035ee1cfb122d39591ae8ee2f
Abhijith Chandra Mergu <a.mergu@ufl.edu>
2023-08-08 07:01:26 -0400

    Added video analysis logic

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from google.cloud import firestore
+import cv2
 
 client = firestore.Client()
+
+
+def get_video_details(path=None):
+    if path is None:
+        return 0, 0
+
+    data = cv2.VideoCapture(path)
+
+    # count the number of frames
+    frames = data.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = data.get(cv2.CAP_PROP_FPS)
+
+    if int(frames) == 0 or int(fps) == 0:
+        return 0, 0, 0
+
+    # calculate duration of the video
+    seconds = round(frames / fps)
+
+    return seconds, frames, fps
 
 
 # Converts strings added to /messages/{pushId}/original to uppercase
@@ -9,15 +29,36 @@ def make_upper_case(data, context):
     collection_path = path_parts[0]
     document_path = "/".join(path_parts[1:])
 
-    affected_doc = client.collection(collection_path).document(document_path)
+    task_doc = client.collection(collection_path).document(document_path)
 
-    cur_value = data["value"]["fields"]["test"]["stringValue"]
-    new_value = cur_value.upper()
+    try:
+        metadata = data["value"]["fields"]["metadata"]["mapValue"]
+        if len(metadata.keys()) != 0:
+            return
+    except Exception as e:
+        print(f"Error in reading task metadata: {e}")
 
-    if cur_value != new_value:
-        print(f"Replacing value: {cur_value} --> {new_value}")
-        affected_doc.update({"original": new_value})
-    else:
-        # Value is already upper-case
-        # Don't perform a second write (which can trigger an infinite loop)
-        print("Value is already upper-case.")
+    try:
+        media_url = data["value"]["fields"]["mediaURL"]["stringValue"]
+    except Exception as e:
+        print(f"Error in reading mediaURL : {e}")
+        print("Analysis aborted")
+        return
+
+    if not media_url:
+        print("Media URL is empty, analysis is aborted")
+        return
+
+    seconds, frames, fps = get_video_details(media_url)
+    print(f"Video analysed: seconds: {seconds}, frames: {frames}, fps: {fps}")
+
+    try:
+        metadata = {
+            "seconds": seconds,
+            "frames": frames,
+            "fps": fps
+        }
+        task_doc.update({"metadata": metadata})
+        print(f"Document successfully updated with metadata : {metadata}")
+    except Exception as e:
+        print(f"Error in updating the document with the metadata: {e}")
