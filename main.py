908b3c020865f768d49336c968fff779581071db
Abhijith Chandra Mergu <a.mergu@ufl.edu>
2023-08-15 12:43:28 -0400

    Updated functions to gen2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from google.cloud import firestore
 import cv2
+from cloudevents.http import CloudEvent
+import functions_framework
+from google.events.cloud import firestore as firestoredata
 
 client = firestore.Client()
 
@@ -27,15 +30,12 @@ def get_video_details(path=None):
     return seconds, frames, fps
 
 
-# Converts strings added to /messages/{pushId}/original to uppercase
 def analyse_video(data, context):
     path_parts = context.resource.split("/documents/")[1].split("/")
     collection_path = path_parts[0]
     document_path = "/".join(path_parts[1:])
 
     task_doc = client.collection(collection_path).document(document_path)
-
-    print("Function triggered")
 
     try:
         metadata = data["value"]["fields"]["metadata"]["mapValue"]
@@ -64,6 +64,55 @@ def analyse_video(data, context):
             "frames": frames,
             "fps": fps,
             "new-param": "new-value"
+        }
+        task_doc.update({"metadata": metadata})
+        print(f"Document successfully updated with metadata : {metadata}")
+    except Exception as e:
+        print(f"Error in updating the document with the metadata: {e}")
+
+
+@functions_framework.cloud_event
+def analyse_video_gen2(cloud_event: CloudEvent) -> None:
+    firestore_payload = firestoredata.DocumentEventData()
+    firestore_payload._pb.ParseFromString(cloud_event.data)
+
+    path_parts = firestore_payload.value.name.split("/")
+    separator_idx = path_parts.index("documents")
+    collection_path = path_parts[separator_idx + 1]
+    document_path = "/".join(path_parts[(separator_idx + 2):])
+
+    print(f"Collection path: {collection_path}")
+    print(f"Document path: {document_path}")
+
+    task_doc = client.collection(collection_path).document(document_path)
+
+    try:
+        metadata = firestore_payload.value.fields["metadata"].map_value
+        if len(metadata.keys()) != 0:
+            return
+    except Exception as e:
+        print(f"Error in reading task metadata: {e}")
+
+    try:
+        media_url = firestore_payload.value.fields["mediaURL"].string_value
+    except Exception as e:
+        print(f"Error in reading mediaURL : {e}")
+        print("Analysis aborted")
+        return
+
+    if not media_url:
+        print("Media URL is empty, analysis is aborted")
+        return
+
+    seconds, frames, fps = get_video_details(media_url)
+    print(f"Video analysed: seconds: {seconds}, frames: {frames}, fps: {fps}")
+
+    try:
+        metadata = {
+            "seconds": seconds,
+            "frames": frames,
+            "fps": fps,
+            "source": "gen2"
         }
         task_doc.update({"metadata": metadata})
         print(f"Document successfully updated with metadata : {metadata}")
